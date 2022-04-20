b8e5236b3a533da61254a38a22ffabf4ba3906a7
Ryan-R-C <ryan.r.c.339ac@gmail.com>
2022-04-20 16:46:09 -0300

    fixed thte exported fields

--- src/modules/cerimoniaData/list/cerimoniaDataListExporterFields.tsx
+++ src/modules/cerimoniaData/list/cerimoniaDataListExporterFields.tsx
@@ -3,32 +3,15 @@ import exporterRenders from 'src/modules/shared/exporter/exporterRenders';
 
 export default [
   {
-    name: 'nomeHomenageado',
-    label: i18n('entities.cerimoniaData.fields.nomeHomenageado'),
+    name: 'nome',
+    label: i18n('entities.cerimoniaData.fields.nome'),
   },
   {
-    name: 'cpf',
-    label: i18n('entities.cerimoniaData.fields.cpf'),
+    name: 'telefone',
+    label: i18n('entities.cerimoniaData.fields.telefone'),
   },
   {
-    name: 'dataCerimoniaData',
-    label: i18n('entities.cerimoniaData.fields.datacerimoniaData'),
+    name: 'email',
+    label: i18n('entities.cerimoniaData.fields.email'),
   },
-  {
-    name: 'responsavel',
-    label: i18n('entities.cerimoniaData.fields.responsavel'),
-  },
-  {
-    name: 'telefoneResponsavel',
-    label: i18n('entities.cerimoniaData.fields.telefoneResponsavel'),
-  },
-  {
-    name: 'emailResponsavel',
-    label: i18n('entities.cerimoniaData.fields.emailResponsavel'),
-  },
-  {
-    name: 'idFuneraria',
-    label: i18n('entities.cerimoniaData.fields.idFuneraria'),
-    render: exporterRenders.relationToOne(),
-  }
 ];
